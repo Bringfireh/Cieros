f63b7e5175c4a019a18b38945a33c3010d2c2403
ngutortsenongo@gmail.com <ngutortsenongo@gmail.com>
2019-07-22 15:16:14 -0700

    Add project files.

---
+++ Cieros/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cieros.Startup))]
+namespace Cieros
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
