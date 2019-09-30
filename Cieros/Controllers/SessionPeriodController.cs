de8189e30c3459d3371e945581b64fcf4cf05d58
ngutortsenongo@gmail.com <ngutortsenongo@gmail.com>
2019-09-30 10:24:29 -0700

    End of Session process completed successfully

--- Cieros/Controllers/SessionPeriodController.cs
+++ Cieros/Controllers/SessionPeriodController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Cieros.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +10,32 @@ namespace Cieros.Controllers
 {
     public class SessionPeriodController : Controller
     {
+        private MyModel db = new MyModel();
+        
         // GET: SessionPeriod
         public ActionResult Index()
         {
+           
+            return View();
+        }
 
+        [HttpPost]
+        public ActionResult Index(string t="")
+        {
+            var students = db.Students.ToList();
+            foreach(var item in students)
+            {
+                string ci = item.StdClass;
+                var rc = db.RankClasses.Find(ci);
+                if (rc != null)
+                {
+                    string nci = rc.NextClass;
+                    item.StdClass = nci;
+                    db.Entry(item).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+            }
+            ViewBag.Msg = "End of Session Successfully runned.";
             return View();
         }
     }
